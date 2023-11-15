1c4a288378af277f57c49e53227f2e46f6d1ea81
Nassif Dauda <nassifdauda@Nassifs-MacBook-Air.local>
2023-11-15 10:59:08 +0000

    Added my project

---
+++ README.md
@@ -0,0 +1,31 @@
+# _{Lango Indeginous Hip-hop}_
+
+#### By _**{Nassif Dauda}**_
+
+#### _{This is a website for Lango indeginous Hip-hop}_
+
+## Technologies Used
+
+* Materialized Framework
+* JavaScript
+* SCSS
+
+## Description
+
+For LIH
+
+## Setup/Installation Requirements
+
+* No setup required
+
+## Known Bugs
+
+* No known bugs
+
+## License
+
+* Github (https://github.com/nassifmd)
+* Twitter (https://twitter.com/nassifdev/)
+* Linkedin (https://www.linkedin.com/in/nassif-dauda/)
+
+Copyright (c) _16/04/2023_ _Nassif Dauda_# Lango-Hip-hop
---
+++ index.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems, true);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.carousel');
+  var instances = M.Carousel.init(elems, true);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.slider');
+  var instances = M.Slider.init(elems, true);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.collapsible');
+  var instances = M.Collapsible.init(elems, true);
+});
+
+
